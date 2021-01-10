a428b6dfe80e900a0cbd5fcbd7c885a6c482316b
sally al sardouk <sally.sardouk98@gmail.com>
2021-01-10 11:09:41 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@
 import React from 'react';
 
 export default class App extends React.Component{
+  state:{
+    
+  }
   componentDidMount(){
     fetch( http://api.open-notify.org/astros.json)
     .then(resp=>resp.json())
